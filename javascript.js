fe0d0df8d63900fb1a4822af04d0fe5f530a4586
“johnrussell <jonnycryptokings@gmail.com>
2023-05-22 19:35:57 -0400

    contact form added P2

--- javascript.js
+++ javascript.js
@@ -1,2 +1,20 @@
-// Add JavaScript functionality here (if needed)
-// This file can be left empty if no JavaScript functionality is required for the about page
+// javascript.js
+
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec';
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  let requestBody = new FormData(form);
+  fetch(scriptURL, { method: 'POST', body: requestBody })
+    .then(response => {
+      alert('Success!', response);
+      submitButton.disabled = false;
+    })
+    .catch(error => {
+      alert('Error!', error.message);
+      submitButton.disabled = false;
+    });
+});
